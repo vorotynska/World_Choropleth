a1601173897f7dff908abeef6230241e497113e5
vorotynska <109528111+vorotynska@users.noreply.github.com>
2023-02-25 00:56:37 +0200

    commit

--- index.js
+++ index.js
@@ -1,4 +1,151 @@
+let countyURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json'
+let educationURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json'
+
+let countyData;
+let educationData;
+
 const svg = d3.select('svg'),
+    width = +svg.attr('width'),
+    height = +svg.attr('heigth')
+
+
+
+
+const colorScheme = d3.schemeReds[6];
+colorScheme.unshift("#eee")
+var colorScale = d3.scaleThreshold()
+    .domain([3, 21, 30, 39, 48, 60])
+    .range(colorScheme);
+
+d3.json(countyURL).then(
+    (data, error) => {
+        if (error) {
+            console.log(log)
+        } else {
+            countyData = topojson.feature(data, data.objects.counties).features
+            console.log(countyData)
+
+            d3.json(educationURL).then(
+                (data, error) => {
+                    if (error) {
+                        console.log(error)
+                    } else {
+                        educationData = data
+                        console.log(educationData)
+
+                        const tooltip = d3.select("body")
+                            .append("div")
+                            .style("visibility", "hidden")
+                            .attr('id', 'tooltip')
+                            .attr("class", "tooltip")
+                            .style("background-color", "beige")
+                            .style("border", "solid")
+                            .style("border-width", "2px")
+                            .style("border-radius", "5px")
+                            .style("padding", "5px")
+
+                        let mouseOver = function (e, d) {
+
+                            d3.selectAll(".Country")
+                                .transition()
+                                .duration(200)
+                                .style("opacity", .5)
+                            d3.select(this)
+                                .transition()
+                                .duration(200)
+                                .style("opacity", 1)
+                                .style("stroke", "black");
+                            tooltip.style("visibility", "visible")
+                                .style("left", e.pageX + 10 + "px")
+                                .style("top", e.pageY - 80 + "px")
+                                .attr("data-date", d[0])
+                                .html(educationData.bachelorsOrHigher + "%");
+                        }
+
+                        let mouseLeave = function (d) {
+                            d3.selectAll(".Country")
+                                .transition()
+                                .duration(200)
+                                .style("opacity", .8)
+                            d3.select(this)
+                                .transition()
+                                .duration(200)
+                                .style("stroke", "transparent");
+                            tooltip
+                                .style('visibility', 'hidden')
+                        }
+
+                        svg.selectAll('path')
+                            .data(countyData)
+                            .enter()
+                            .append('path')
+                            .attr('d', d3.geoPath())
+                            .attr('class', 'county')
+                            .attr('fill', (countyDataItem) => {
+                                let id = countyDataItem['id']
+                                let county = educationData.find((item) => {
+                                    return item['fips'] === id
+                                })
+                                let percentage = county['bachelorsOrHigher']
+                                return colorScale(percentage)
+
+                            })
+                            .attr('data-fips', (countyDataItem) => {
+                                return countyDataItem['id']
+                            })
+                            .attr('data-education', (countyDataItem) => {
+                                let id = countyDataItem['id']
+                                let county = educationData.find((item) => {
+                                    return item['fips'] === id
+                                })
+                                let percentage = county['bachelorsOrHigher']
+                                return percentage
+
+                            })
+                            .on("mouseover", mouseOver)
+                            .on("mouseleave", mouseLeave)
+
+
+
+                        //Legend
+                        const g = svg.append('g')
+                            .attr('class', "legendThreshold")
+                            .attr('id', 'legend')
+                            .attr('transform', "translate(20,20)");
+
+                        g.append('text')
+                            .attr('id', 'description')
+                            .attr('class', 'caption')
+                            .attr('x', 0)
+                            .attr('y', -6)
+                            .text('Education');
+
+                        const labels = ['3%', '21%', '30%', '39%', '48%', '60%', '> 60%'];
+                        const legend = d3.legendColor()
+                            .labels(d => labels[d.i])
+                            .shapePadding(4)
+                            .scale(colorScale);
+                        svg.select('.legendThreshold')
+                            .call(legend);
+
+                        g.append("text")
+                            .attr('id', 'title')
+                            .attr("x", (width / 2))
+                            .attr("y", height + 10)
+                            .attr("text-anchor", "middle")
+                            .style("font-size", "32px")
+                            .text("United States Educational Attainment");
+
+
+                    }
+
+                }
+            )
+        }
+    }
+)
+
+/*const svg = d3.select('svg'),
     width = +svg.attr('width'),
     height = +svg.attr('heigth');
 
@@ -64,4 +211,4 @@ Promise.all([
         .attr("class", function (d) {
             return "Country"
         })
-})
+})*/
